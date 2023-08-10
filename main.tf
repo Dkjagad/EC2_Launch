ae949b73e21c7016fa00bd5ecec93745be0c3063
Dkjagad <111210023+Dkjagad@users.noreply.github.com>
2023-08-10 02:14:44 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "us-east-1"  
 }
 
-resource "aws_instance" "f" {
+resource "aws_instance" "mansi" {
   ami           = "ami-0f34c5ae932e6f0e4" 
   instance_type = "t2.micro"
   tags = {
